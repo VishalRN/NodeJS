c049420f8f3cbcf50de987a7dedc20a31a434469
Vishal.R.Nadagoudar <137093870+VishalRN@users.noreply.github.com>
2025-06-22 19:54:18 +0530

    yes

--- 87 - Working with Files fs and path Modules/aboutpath.js
+++ 87 - Working with Files fs and path Modules/aboutpath.js
@@ -6,4 +6,4 @@ console.log(path.extname(myPath))
 console.log(path.dirname(myPath))
 console.log(path.basename(myPath))
 
-console.log(path.join("c:/", "programs\\harry.txt"))
+console.log(path.join("c:/", "programs\\vishal.txt"))
